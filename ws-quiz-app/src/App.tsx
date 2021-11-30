9f78e509103d27f520a4508b1ab599b2d06faaa2
Eva Mélinand <eva.melinand@gmail.com>
2021-11-30 16:09:36 +0100

    settings the start

--- ws-quiz-app/src/App.tsx
+++ ws-quiz-app/src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { fetchQuizQuestions } from './API';
+
+import { QuestionState, Difficulty } from './API';
+
+import QuestionCard from './components/QuestionCard';
+
+type AnswerObject = {
+  question: string,
+  answers: string[],
+  correct: boolean,
+  correctAnswer: string,
+}
+
+const TOTAL_QUESTION = 10;
 
 const App = () => {
+const [loading, setLoading] = useState(false);
+const [questions, setQuestions] = useState<QuestionState[]>([]);
+const [number, setNumber] = useState(0);
+const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+const [score, setScore] = useState(0);
+const [gameOver, setGameOver] = useState(true);
+
 
 const startQuiz = async () => {
+setLoading(true);
+setGameOver(false);
+
+const newQuestion = await fetchQuizQuestions(
+  TOTAL_QUESTION,
+  Difficulty.EASY
+);
+
+setQuestions(newQuestion);
+setScore(0);
+setUserAnswers([]);
+setNumber(0);
+setLoading(false);
 
 }
 
@@ -17,9 +51,19 @@ const nextQuestion = () => {
   return (
     <div className="App">
       <h1>Quiz</h1>
-      <button className="start" onClick={startQuiz}>Starting the quiz</button>
+      {gameOver || userAnswers.length === TOTAL_QUESTION ? //The button will appear only if the game is over or if the user answered all the questions
+      (<button className="start" onClick={startQuiz}>Starting the quiz</button>) : null}
       <p className="score">Score: </p>
       <p>Loading questions...</p>
+      {/* <QuestionCard 
+      QuestionNb={number + 1}
+      totalQuestions={TOTAL_QUESTION}
+      question={questions[number].question}
+      answers={questions[number].answers}
+      userAnswer={userAnswers ? userAnswers[number] : undefined}
+      callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>Next question</button>
     </div>
   );
 }
--- ws-quiz-app/src/components/QuestionCard.tsx
+++ ws-quiz-app/src/components/QuestionCard.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
-const QuestionCard = () => <div>QuestionCard</div>
+type Card = {
+    question: string,
+    answers: string[],
+    callback: any,
+    userAnswer: any,
+    QuestionNb: number,
+    totalQuestions: number
+}
+
+
+const QuestionCard: React.FC<Card> = ({question, answers, callback, userAnswer, QuestionNb, totalQuestions}) => (
+(<div>
+    <p className="number">Question: {QuestionNb} / {totalQuestions}</p>
+    <p dangerouslySetInnerHTML={{__html: question}}></p>
+    <div>
+        {answers.map (answer => (
+            <div>
+                <button disabled={userAnswer} onClick={callback}>
+                    <span dangerouslySetInnerHTML={{__html: answer}} />
+                </button>
+            </div>
+        ))}
+    </div>
+</div>)
+)
 
 export default QuestionCard;
